3fc16fa2f79eff701dd2f456e95a50229460bb28
Vinicius <viniciusgama@outlook.com>
2014-08-21 15:15:58 -0300

    Versao final Tarefa 02

--- tarefa_02.ino
+++ tarefa_02.ino
@@ -10,31 +10,34 @@
 #define BUTTON2_PIN 3
 
 typedef enum {
-  State0,
-  State1,
-  InState1,
-  State2,
-  InState2,
-  State3,
+  Idle,
+  Button1Pressed,
+  InButton1Pressed,
+  Button2Pressed,
+  InButton2Pressed,
+  AllButtonsPressed,
 } State;
 
 State state_machine;
 unsigned long old_time;
 unsigned long new_time;
 unsigned long wait_time;
-unsigned long debounce_delay = 50; 
 int led_state;
 
-void WaitingInputs(int button1, int button2)
+const unsigned long debounce_delay = 50;
+const unsigned long change_time = 100;
+const unsigned long stop_time = 500;
+
+void IdleFunc(int button1, int button2)
 {
   if (button1 == HIGH && button2 == HIGH) {
-    state_machine = State3;
+    state_machine = AllButtonsPressed;
   }
   else if (button1 == HIGH) {
-    state_machine = State1;
+    state_machine = Button1Pressed;
   }
   else if (button2 == HIGH) {
-    state_machine = State2;
+    state_machine = Button2Pressed;
   }
 
   if (new_time >= old_time + wait_time) {
@@ -49,7 +52,7 @@ int IsDebounce()
   return (new_time - debounce_delay <= old_time);
 }
 
-void VerifyState3(int button1, int button2)
+void VerifyAllButtonsPressed(int button1, int button2)
 {
   if (IsDebounce()) {
     return;
@@ -59,55 +62,55 @@ void VerifyState3(int button1, int button2)
     while(1);
   }
   else {
-    state_machine = State0;
+    state_machine = Idle;
   }
 }
 
-void VerifyState1(int button1, int button2)
+void VerifyButton1Pressed(int button1, int button2)
 {
   if (IsDebounce()) {
     return;
   }
 
-  if (button1 == HIGH && button2 == LOW && state_machine != InState1) {
-    state_machine = InState1;
-    wait_time -= 100;
+  if (button1 == HIGH && button2 == LOW && state_machine != InButton1Pressed) {
+    state_machine = InButton1Pressed;
+    wait_time -= change_time;
   }
   else if (button1 == LOW && button2 == LOW) {
-    state_machine = State0;
+    state_machine = Idle;
   }
   else if (button1 == LOW && button2 == HIGH) {
-    state_machine = State2;
+    state_machine = Button2Pressed;
   }
-  else  if (button1 == HIGH && button2 == HIGH && new_time - old_time <= 500) {
-    state_machine = State3;
+  else  if (button1 == HIGH && button2 == HIGH && new_time - old_time <= stop_time) {
+    state_machine = AllButtonsPressed;
   }
 }
 
-void VerifyState2(int button1, int button2)
+void VerifyButton2Pressed(int button1, int button2)
 {
   if (IsDebounce()) {
     return;
   }
 
-  if (button2 == HIGH && button1 == LOW && state_machine != InState2) {
-    state_machine = InState2;
-    wait_time += 100;
+  if (button2 == HIGH && button1 == LOW && state_machine != InButton2Pressed) {
+    state_machine = InButton2Pressed;
+    wait_time += change_time;
   }
   else if (button2 == LOW && button1 == LOW) {
-    state_machine = State0;
+    state_machine = Idle;
   }
   else if (button2 == LOW && button1 == HIGH) {
-    state_machine = State1;
+    state_machine = Button1Pressed;
   }
-  else if (button2 == HIGH && button1 == HIGH && new_time - old_time <= 500) {
-    state_machine = State3;
+  else if (button2 == HIGH && button1 == HIGH && new_time - old_time <= stop_time) {
+    state_machine = AllButtonsPressed;
   }
 }
 
 void setup()
 {
-  state_machine = State0;
+  state_machine = Idle;
   old_time = millis();
   wait_time = 1000;
   led_state = HIGH;
@@ -124,26 +127,26 @@ void loop()
   int button2 = digitalRead(BUTTON2_PIN);
 
   switch(state_machine) {
-    case State0:
-      WaitingInputs(button1, button2);
+    case Idle:
+      IdleFunc(button1, button2);
     break;
 
-    case State1:
-    case InState1:
-      VerifyState1(button1, button2);
+    case Button1Pressed:
+    case InButton1Pressed:
+      VerifyButton1Pressed(button1, button2);
     break;
 
-    case State2:
-    case InState2:
-      VerifyState2(button1, button2);
+    case Button2Pressed:
+    case InButton2Pressed:
+      VerifyButton2Pressed(button1, button2);
     break;
 
-    case State3:
-      VerifyState3(button1, button2);
+    case AllButtonsPressed:
+      VerifyAllButtonsPressed(button1, button2);
     break;
 
     default:
-      state_machine = State0;
+      state_machine = Idle;
     break;
   }
 }
